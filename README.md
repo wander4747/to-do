6c10520d943fbdc73a2d08b79632e57070cbd131
Wander Douglas <wander.douglas14@gmail.com>
2020-09-03 08:52:07 -0300

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-# to-do-list
+# Gerenciador de tarefas - Cod3r
 
-## Project setup
-```
-yarn install
-```
+## 💻 Projeto
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+Um projeto simples de gerenciador de tarefas feito com VueJs.
 
-### Compiles and minifies for production
-```
-yarn build
-```
+- [Link do curso VueJs](https://www.cod3r.com.br/courses/vue)
 
-### Lints and fixes files
-```
-yarn lint
+![screen](https://user-images.githubusercontent.com/6461792/92111306-817b4800-edc2-11ea-8254-21cde488e35f.gif)
+
+## 🧑‍💻 Bora rodar o projeto?!
+
+```sh
+$ git clone https://github.com/wander4747/to-do.git
+$ cd to-do
+$ npm run serve
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Contatos
+
+[![Github Badge](https://img.shields.io/badge/-Github-000?style=flat-square&logo=Github&logoColor=white&link=https://github.com/wander4747)](https://github.com/wander4747)
+[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/wander-douglas/)](https://www.linkedin.com/in/wander-douglas/)
+[![Whatsapp Badge](https://img.shields.io/badge/-Whatsapp-4CA143?style=flat-square&labelColor=4CA143&logo=whatsapp&logoColor=white&link=https://api.whatsapp.com/send?phone=5531993326096&text=Hello!)](https://api.whatsapp.com/send?phone=5531993326096&text=Hello!)
+[![Gmail Badge](https://img.shields.io/badge/-Gmail-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:wander.douglas14@gmail.com)](mailto:wander.douglas14@gmail.com)
+
+Feito com muito ❤️☕👨🏻‍💻 por Wander Douglas
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tarefas</h1>
+    <TasksProgress :progress="progress" />
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChanged="toggleTaskState" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TasksProgress from "./components/TasksProgress.vue";
+import NewTask from "./components/NewTask.vue";
+import TaskGrid from "./components/TaskGrid.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TasksProgress, NewTask, TaskGrid },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  computed: {
+    progress() {
+      const total = this.tasks.length;
+      const done = this.tasks.filter((t) => !t.pending).length;
+      return Math.round((done / total) * 100) || 0;
+    },
+  },
+  watch: {
+    tasks: {
+      deep: true,
+      handler() {
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      },
+    },
+  },
+  methods: {
+    addTask(task) {
+      const sameName = (t) => t.name === task.name;
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true,
+        });
+      }
+      console.log(this.tasks);
+    },
+    deleteTask(i) {
+      this.tasks.splice(i, 1);
+    },
+    toggleTaskState(i) {
+      this.tasks[i].pending = !this.tasks[i].pending;
+    },
+  },
+  created() {
+    const json = localStorage.getItem("tasks");
+    const array = JSON.parse(json);
+    this.tasks = Array.isArray(array) ? array : [];
+  },
+};
 </script>
 
 <style>
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
 }
 </style>
